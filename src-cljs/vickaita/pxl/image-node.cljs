53517d6fff6d417b8e8202c6ced7d7a5404f333e
Vick Aita <vickaita@gmail.com>
2013-05-24 18:39:46 -0400

    Changes to image-node.

--- src-cljs/vickaita/pxl/image-node.cljs
+++ src-cljs/vickaita/pxl/image-node.cljs
@@ -1,10 +1,14 @@
 (ns vickaita.pxl.image-node
   (:require [vickaita.raster.core :as raster]))
 
+(def id-counter (atom 0))
+
+(defn- guid [] (str "image-node-" (swap! id-counter inc)))
+
 (defn image-node
   ([data] (image-node data nil))
-  ([data parent] {:parent parent
-                  :image-data data
+  ([data parent] {:parent (or (:id parent) (guid))
+                  :id (guid)
                   :width (raster/width data)
                   :height (raster/height data)
                   :data (raster/data data)}))
