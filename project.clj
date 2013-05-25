efddd64f4436aeb28b17fe418b204f168a11cb7b
Vick Aita <vickaita@gmail.com>
2013-05-24 22:48:07 -0400

    Bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raster "0.1.0-SNAPSHOT"
+(defproject raster "0.1.0"
   :description "A library for working with ImageData in ClojureScript."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
