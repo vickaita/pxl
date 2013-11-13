bc9159ece99f2a9b62c3a2e66e238c084ad6efa1
Vick Aita <vickaita@gmail.com>
2013-11-12 20:56:18 -0500

    Update ClojureScript version and add core.async.

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1806"]
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
                  [com.cemerick/clojurescript.test "0.0.3"]
+                 [org.clojure/clojure "1.5.1"]
                  [clj-aws-s3 "0.3.3"]
                  [ring "1.1.8"]
                  [ring-server "0.2.8"]
@@ -20,17 +22,20 @@
   :plugins  [[lein-cljsbuild "0.3.2"]
              [lein-ring "0.8.2"]]
   :source-paths ["src/clj"]
-  :cljsbuild {:builds
-              [{:source-paths ["src/cljs"]
-                :id "dev"
-                :compiler {:pretty-print true
-                           :output-to "resources/public/js/main.js"
-                           ;:source-map "resources/public/js/main.map"  
-                           :optimizations :whitespace}}
-               {:source-paths ["src/cljs"]
-                :id "prod"
+  :cljsbuild {:repl-listen-port 9201 
+              :builds
+              [{:id "dev"
+                :source-paths ["src/cljs"]
+                :compiler {:output-to "main.js"
+                           :output-dir "resources/public/js/"
+                           :pretty-print true
+                           :source-map true
+                           :optimizations :none}}
+               {:id "prod"
+                :source-paths ["src/cljs"]
                 :compiler {:pretty-print false
-                           :output-to "resources/public/js/main.js"
-                           :optimizations :advanced}}]
-              :repl-listen-port 9201}
+                           :output-to "main.js"
+                           :output-dir "resources/public/js/"
+                           :optimizations :advanced}}]}
   :ring {:handler vickaita.pxl.core/app})
+
